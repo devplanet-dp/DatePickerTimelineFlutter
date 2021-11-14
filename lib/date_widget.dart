916d879e8a74f5ea18c82daf46defffad719bfb6
DEV PLANET <59248870+devplanet-dp@users.noreply.github.com>
2021-11-14 11:03:36 +0530

    Update date_widget.dart

--- lib/date_widget.dart
+++ lib/date_widget.dart
@@ -44,12 +44,11 @@ class DateWidget extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: <Widget>[
-              Text(new DateFormat("MMM", locale).format(date).toUpperCase(), // Month
-                  style: monthTextStyle),
+        Text(new DateFormat("E", locale).format(date).toUpperCase(), // WeekDay
+                  style: dayTextStyle)
               Text(date.day.toString(), // Date
                   style: dateTextStyle),
-              Text(new DateFormat("E", locale).format(date).toUpperCase(), // WeekDay
-                  style: dayTextStyle)
+             
             ],
           ),
         ),
